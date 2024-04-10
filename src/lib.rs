8555391b5518b379cffc65c1bbf8eda50de37e27
sepehr mokhtar <113509443+itssepsi@users.noreply.github.com>
2024-04-10 23:33:58 +0200

    commit

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,16 @@
 pub fn is_leap_year(year: u64) -> bool {
     unimplemented!("true if {year} is a leap year")
-}
-//itssepsi
+        if year % 100 == 0 && year % 400 == 0 {
+            return true;
+        }
+        if year % 100 == 0 {
+            return false;
+        }   
+        if year % 4 == 0 {
+            return true;
+        }   
+    
+        return false;
+    }
+    //Hello professor, i'm not done with this code but i'll appreciate any comment 
+    //from you
